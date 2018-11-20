647307297c07d702dd516a075ba673b1e451d117
kamteo <kamin.teo@gmail.com>
2018-11-20 10:08:13 +0000

    Hello Contract

---
+++ migrations/2_deploy_hello.js
@@ -0,0 +1,7 @@
+const hello = artifacts.require('./HelloWorld');
+const helloSettings = {
+  name: "Filip"
+}
+module.exports = function(deployer) {
+  deployer.deploy(hello, helloSettings.name);
+};
